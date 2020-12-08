6e47a42c45f57e2568a331f1ad964e98a396533e
David Redmon <54488667+DMR3DY@users.noreply.github.com>
2020-12-07 22:38:05 -0500

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,36 @@
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+#importing dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# Setting up Training and Testing Datasets
+array = dataset.values
+X = array[:, 0:4]
+Y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.20, random_state = 1)
+
+#Training and Testing using a Decision Tree Classification Method
+result = []
+kfold = StratifiedKFold(n_splits=25, random_state=1, shuffle=True)
+cv_results = cross_val_score(DecisionTreeClassifier(), X_train, Y_train, cv=kfold, scoring='accuracy')
+result.append(cv_results)
+print('Accuracy: %f' % cv_results.mean())
+
